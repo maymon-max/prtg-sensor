7b625966f3b98e7bda3b61f98dc09b243cf4d06e
Maksim Petrov <likempv@gmail.com>
2022-12-22 12:31:47 +0300

    setup.py modify

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup
 
 if __name__ == '__main__':
+
+    with open('README.md') as f:
+        README = f.read()
+
     setup(
         name='prtg-sensor',
-        version='0.0.1',
+        version='1.0.0',
         description='Simple data generator for advanced PRTG sensors',
+        long_description=README,
+        long_description_content_type='text/markdown',
         author='Max Maymon',
         url='https://github.com/maymon-max/prtg-sensor.git',
         license='MIT',
+        keywords='prtg',
         packages=['prtg_sensor'],
         classifiers=[
+            'Programming Language :: Python',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
